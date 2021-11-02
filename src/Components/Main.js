4134a2556286d5a67f630aad36a657354dc27b5a
KimrAndrew <andrewrk1997@gmail.com>
2021-11-02 14:55:43 -0700

    app now retrieves location objects from api

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -17,31 +17,36 @@ export default class Main extends Component {
         }
     }
 
-    assembleLocationUrl = () => `${this.base}${this.key}&q=${this.state.searchQuery}${this.format}`;
+    assembleLocationUrl = () => `${this.base}${this.key}&q=${this.state.searchText}${this.format}`;
 
-    assembleMapUrl = () => `https://maps.locationiq.com/v3/staticmap?key=${this.key}center=${this.state.cityData.lon},${this.state.cityData.lat}zoom=9`
+    //assembleMapUrl = () => `https://maps.locationiq.com/v3/staticmap?${this.key}center=${this.state.cityData.lon},${this.state.cityData.lat}zoom=9`
 
     changeHandler = event => {
-        this.setState({searchText:event.target.value});
+        this.setState({searchText: event.target.value});
     };
-    searchHandler =  async () => {
+
+    searchHandler = async () => {
         this.setState({
             searchQuery: this.state.searchText,
-            locationUrl: this.assembleLocationUrl(),
-            mapUrl: this.assembleMapUrl()
+            locationUrl:  await this.assembleLocationUrl()
+            //mapUrl: this.assembleMapUrl()
         });
-        this.setState({cityData: this.getCityData()});
-        this.setState({map: this.getMapData()});
+        console.log(this.state.locationUrl);
+        let cityData = await axios.get(this.state.locationUrl);
+        cityData = cityData.data[0];
+        this.setState({cityData: cityData});
+        //this.setState({map: this.getMapData()});
+        console.log(this.state.cityData);
         
     }
 
-    getCityData = async () => await axios.get(this.state.locationUrl);
+    //getCityData = await axios.get(this.state.locationUrl);
 
     getMapData = async () => await axios.get(this.state.mapUrl);
     render() {
         return(
             <>
-                <input onChange={this.changeHandler}></input>
+                <input onChange={this.changeHandler} value={this.state.searchText}></input>
                 <p>{this.state.cityData.name}</p>
                 <p>{this.state.cityData.lon}</p>
                 <p>{this.state.cityData.latr}</p>
