59c94d4fbc5baad879a31f6dbd13829e4f408d6e
KimrAndrew <andrewrk1997@gmail.com>
2021-11-01 20:25:25 -0700

    added methods for getting api data

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -12,11 +12,14 @@ export default class Main extends Component {
             searchQuery: '',
             locationUrl:'',
             mapUrl:'',
-            cityData: {}
+            cityData: {},
+            map: ''
         }
     }
 
     assembleLocationUrl = () => `${this.base}${this.key}&q=${this.state.searchQuery}${this.format}`;
+
+    assembleMapUrl = () => `https://maps.locationiq.com/v3/staticmap?key=${this.key}center=${this.state.cityData.lon},${this.state.cityData.lat}zoom=9`
 
     changeHandler = event => {
         this.setState({searchText:event.target.value});
@@ -25,17 +28,23 @@ export default class Main extends Component {
         this.setState({
             searchQuery: this.state.searchText,
             locationUrl: this.assembleLocationUrl(),
+            mapUrl: this.assembleMapUrl()
         });
-
-        let response = await axios.get(this.state.locationUrl);
-        this.setState({cityData:response.data[0]});
-        console.log(this.state.cityData);
+        this.setState({cityData: this.getCityData()});
+        this.setState({map: this.getMapData()});
+        
     }
+
+    getCityData = async () => await axios.get(this.state.locationUrl);
+
+    getMapData = async () => await axios.get(this.state.mapUrl);
     render() {
         return(
             <>
                 <input onChange={this.changeHandler}></input>
-                <p>{this.assembleLocationUrl()}</p>
+                <p>{this.state.cityData.name}</p>
+                <p>{this.state.cityData.lon}</p>
+                <p>{this.state.cityData.latr}</p>
                 <button onClick={this.searchHandler}>Search</button>
             </>
         );
