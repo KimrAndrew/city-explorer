634da82c9bf772338b6befb985fe268f5e7cb773
KimrAndrew <andrewrk1997@gmail.com>
2021-11-05 18:36:58 -0700

    componetized front end

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -4,7 +4,8 @@ import Container from 'react-bootstrap/Container';
 import SearchForm from './SearchForm.js';
 import LocationCard from './LocationCard.js';
 import Weather from './Weather';
-import Movie from './Movie';
+import Movies from './Movies';
+import Alert from 'react-bootstrap/Alert';
 
 export default class Main extends Component {
     constructor(props) {
@@ -12,8 +13,10 @@ export default class Main extends Component {
 
         this.state = {
             cityData: {},
+            cityName: '',
             map: '',
-            weatherList: <></>,
+            weatherList: [],
+            movieList: [],
             error: false
         }
     }
@@ -22,41 +25,71 @@ export default class Main extends Component {
         this.setState({
             cityData:{},
             map: '',
+            cityName: '',
+            weatherList: [],
+            movieList: [],
             error: false
         });
         try {
+            this.getLocationData(query);
+
+            //let movieData = await axios.get(`${process.env.REACT_APP_API}movies/?city_name=${cityName}`);
+            //movieData=movieData.data.map(movie => movie.title);
+            //movieData = movieData.data;
+
+            //this.setState({
+            //    movieList: movieData
+            //});
+            console.log(this.state.movieList);
+
+        } catch(error) {
+            this.handleError(error);
+        }
+    }
+
+    getLocationData = async(query) => {
+        try {
             let cityData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_KEY}&q=${query}&format=json`);
             cityData = cityData.data[0];
-            console.log(cityData.lat);
-            console.log(cityData.lon);
-
+            let cityName = cityData.display_name.split(',')[0];
             this.setState({
                 cityData: cityData,
-                map: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_KEY}&center=${cityData.lat},${cityData.lon}&zoom=10`
-            });
+                map: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_KEY}&center=${cityData.lat},${cityData.lon}&zoom=10`,
+                cityName: cityName
+            },this.getWeather);
+        } catch(error) {
+            this.handleError(error)
+        }
+    }
 
-            let weatherData = await axios.get(`${process.env.REACT_APP_API}weather/?lat=${cityData.lat}&lon=${cityData.lon}`);
-            let weatherList = weatherData.data.map((el) => {
-                return <li key={el.date}> {`date: ${el.date} description: ${el.description}`}</li>
-            });
+    handleError = (error) => {
+        this.setState({error:error});
+    }
 
-            console.log(this.state.cityData);
+    getWeather = async () => {
+        try {
+            let weatherData = await axios.get(`${process.env.REACT_APP_API}weather/?lat=${this.state.cityData.lat}&lon=${this.state.cityData.lon}`);
+
             this.setState({
-                weatherList: weatherList
-            });
-            let cityName = this.state.cityData.display_name.split(',')[0];
-            console.log("city name:" + cityName);
-            let movieData = await axios.get(`${process.env.REACT_APP_API}movies/?city_name=${cityName}`);
-            movieData=movieData.data.map(movie => movie.title);
+                weatherList: weatherData.data
+            },this.getMovies);
+        } catch (error) {
+            this.handleError(error);
+        }
+    }
 
-            //console.log(cityData);
+    getMovies = async () => {
+        try {
+            console.log(`cityname: ${this.state.cityName}`);
+            let movieData = await axios.get(`${process.env.REACT_APP_API}movies/?city_name=${this.state.cityName}`);
+            movieData = movieData.data;
             console.log(movieData);
             this.setState({
                 movieList: movieData
             });
-
+            console.log("state: " + this.state.movieList);
         } catch(error) {
-            this.setState({error:true});
+            this.setState({error:error});
         }
     }
 
@@ -66,12 +99,14 @@ export default class Main extends Component {
                 <SearchForm searchHandler={this.searchHandler} value={this.state.searchText}/>
                 <Container> 
                     { this.state.cityData.display_name && <LocationCard cityData={this.state.cityData} map={this.state.map}/> }
-                    { this.state.weatherList && <Weather weatherList={this.state.weatherList}/> }
-                    { this.state.movieList && <Movie movieList={this.state.movieList}/> }
+                    { this.state.weatherList && <Weather weatherList={this.state.weatherList }/> }
+                    { this.state.movieList.length > 0 && <Movies movieList={this.state.movieList}/>}
 
                 {
                     this.state.error &&
-                    <p>Error: Location not found</p>
+                    <Alert variant='danger' onClose={() => this.setState({error:false})} dismissable>
+                        <Alert.Heading>{this.state.error.name + ': ' + this.state.error.message}</Alert.Heading>
+                    </Alert>
                 }
                 </Container>
             </>
--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,9 +1,11 @@
 import { Component } from "react";
+import Row from 'react-bootstrap/Row';
+import WeatherDay from './WeatherDay.js';
 
 export default class Weather extends Component {
 
     render() {
-        return <ul>{this.props.weatherList}</ul>;
+        return <Row xs={1} sm={2} md={3}>{this.props.weatherList.map((day,idx) => <WeatherDay data={day} key={idx}/>)}</Row>;
     }
-    
+
 }
