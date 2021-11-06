c924590d3dc7c00d8d5a41826a25d45088f53bae
KimrAndrew <andrewrk1997@gmail.com>
2021-11-05 18:48:12 -0700

    added keys to each movie card

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -7,7 +7,7 @@ export default class Movies extends Component {
     render() {
         console.log(this.props.movieList);
         return (
-            <Row xs={1} sm={2} md={3}>{this.props.movieList.map(movie => <Movie movie={movie}/>)} </Row>
+            <Row xs={1} sm={2} md={3}>{this.props.movieList.map(movie => <Movie movie={movie} key={movie.title}/>)} </Row>
         )
     }
     
